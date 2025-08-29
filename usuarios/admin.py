5d2721533c60f9dec7d9a5be6ac2fc375843e8ab
Victor Nunes <victorgabrielnunes21@gmail.com>
2025-08-29 14:44:12 -0400

    Update admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import ItemCardapio
 # Register your models here.
 
-admin.site.register(ItemCardapio)# Registra o modelo Nota no admin do Django
-# Isso permite que você gerencie as notas através da interface administrativa do Django.
+admin.site.register(ItemCardapio)# Registra o modelo ItemCardapio no admin do Django
+# Isso permite que você gerencie os itens através da interface administrativa do Django.
